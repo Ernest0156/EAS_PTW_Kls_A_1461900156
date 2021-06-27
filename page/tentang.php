f008ade1fc1fbae39057906e3d26639d891d2f56
ErnestPatrolan2000 <ernestpatrolan@gmail.com>
2021-06-28 06:56:34 +0700

    EAS_PTW_Kls_A_1461900156

---
+++ page/tentang.php
@@ -0,0 +1,58 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/ernest.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Ernest Patrolan</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 1461900156</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: S1 Teknik Informatika</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: Sepak Bola, Naik Gunung, Futsal, Berenang, Menembak</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Minat</b></li>
+        </td>
+        <td>: Membaca</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
